46b66dfa8589ee50d14168bbbfb743df78ae7f6f
Elvira <eliasufa@yahoo.com>
2022-09-09 02:23:35 +0500

    Задание 1 готово

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintTwoDimArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Заролните массив любыми вещественными числами через Enter: ");
+double[,] nums = new double[m, n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        nums[i,j] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+Console.WriteLine("Полученный двумерный массив: ");
+PrintTwoDimArray (nums);
